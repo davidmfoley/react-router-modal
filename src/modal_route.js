d48cb2308f33407c1103f57c0853ce4336ed81a1
Dave Foley <davidmfoley@gmail.com>
2017-06-04 11:33:14 -0700

    Auto-nav-to-parent on backdrop click

--- src/modal_route.js
+++ src/modal_route.js
@@ -1,9 +1,11 @@
 // @flow
 import React from 'react';  // eslint-disable-line no-unused-vars
-import { Route } from 'react-router-dom';
+import { Route, withRouter } from 'react-router-dom';
 import Modal from './modal';
 
 type Props = {
+  match: { url: string },
+  history: { push: Function },
   path: string,
   component: any,
   exact?: boolean,
@@ -16,12 +18,25 @@ function getStackOrder(match) {
   return match.url.length - 10000;
 }
 
-export default function ModalRoute({ path, className, component, exact, props }: Props): React$Element<*> {
+function ModalRoute({ path, className, component, exact, props, match, history }: Props): React$Element<*> {
   const Component = component;
+
+  const parentPath = match.url;
+  const navToParent = () => {
+    history.push(parentPath);
+  };
 
   return (
     <Route path={path} exact={exact} render={({match, location, history}) => (
-      <Modal component={component} props={{...props, match, location, history}} className={className} stackOrder={getStackOrder(match)}/>
+      <Modal
+        component={component}
+        props={{...props, match, location, history}}
+        className={className}
+        stackOrder={getStackOrder(match)}
+        onBackdropClick={navToParent}
+      />
     )} />
   );
 }
+
+export default withRouter(ModalRoute);
