1f1f42444e838a7dbd6f6de14dc149e053551fa8
Dave Foley <davidmfoley@gmail.com>
2017-06-11 10:33:20 -0700

    Update docs

--- src/index.js
+++ src/index.js
@@ -3,4 +3,37 @@ import ModalContainer from './modal_container';
 import ModalLink from './modal_link';
 import ModalRoute from './modal_route';
 
+/*
+* @example <caption>TL;DR</caption>
+*import { ModalContainer, ModalRoute } from 'react-router-modal';
+*import { BrowserRouter, Link } from 'react-router-dom';
+
+*function FooModal() {
+*  return <div>FOO</div>;
+*}
+
+*function BarModal() {
+*  return <div>BAR</div>;
+*}
+
+*function Example() {
+* return (
+*   <BrowserRouter>
+*     <div>
+*       <Link to='/foo/bar'>Bar on top of Foo</Link>
+*       <Link to='/bar/foo'>Foo on top of Bar</Link>
+*
+*       <Link to='/foo'>just foo</Link>
+*       <Link to='/bar'>just bar</Link>
+*
+*       <ModalRoute component={FooModal} path='*\/foo' className='test-modal test-modal-foo'/>
+*       <ModalRoute component={BarModal} path='*\/bar' className='test-modal test-modal-bar'/>
+*
+*       <ModalContainer />
+*     </div>
+*   </BrowserRouter>
+* );
+*}
+*/
 export default {Modal, ModalContainer, ModalLink, ModalRoute};
+
