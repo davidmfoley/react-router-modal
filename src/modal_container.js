4e0f9a917a0cce01a7bdb54c44179101f79bff44
Dave Foley <davidmfoley@gmail.com>
2017-06-22 15:28:10 -0700

    Really check for document

--- src/modal_container.js
+++ src/modal_container.js
@@ -83,50 +83,50 @@ type State = {
 }
 
 /**
-* Container for rendered modals.
-*
-* This should be included in your react app as one of the last elements before the closing body tag.
-* When modals are rendered, they live inside this container.
-* When no modals are shown, nothing is rendered into the DOM.
-*
-* @param {Props} props
-* @param {String} [props.modalClassName=react-router-modal__modal] class name to apply to modals
-* @param {String} [props.backdropClassName=react-router-modal__backdrop] class name to apply to modal backdrops
-* @param {String} [props.containerClassName=react-router-modal__container] class name to apply to the container itself
-* @param {String} [props.bodyModalClassName=react-router-modal__modal-open] class name to apply to the <body /> when any modals are shown
-*
-* @example <caption>Using default class names</caption>
-*
-* <ModalContainer />
-*
-* @example <caption>Overriding the default class names</caption>
-*
-* <ModalContainer
-*   bodyModalOpenClassName='modal-open'
-*   containerClassName='modal-container'
-*   backdropClassName='modal-backdrop'
-*   modalClassName='modal'
-* />
-*
-*
-* @example <caption>DOM structure</caption>
-* // Note that modals are made "modal" via CSS styles, and end up rendered like the following in the DOM (with two modals, for example):
-* <div className={containerClassName}>
-*   <div>
-*     <div className={backdropClassName} />
-*     <div className={modalClassName}>
-*       .. bottom-most modal contents ..
-*     </div>
-*   </div>
-*   <div>
-*     <div className={backdropClassName} />
-*     <div className={modalClassName}>
-*       .. top-most modal contents ..
-*     </div>
-*   </div>
-* </div>
-*
-*/
+ * Container for rendered modals.
+ *
+ * This should be included in your react app as one of the last elements before the closing body tag.
+ * When modals are rendered, they live inside this container.
+ * When no modals are shown, nothing is rendered into the DOM.
+ *
+ * @param {Props} props
+ * @param {String} [props.modalClassName=react-router-modal__modal] class name to apply to modals
+ * @param {String} [props.backdropClassName=react-router-modal__backdrop] class name to apply to modal backdrops
+ * @param {String} [props.containerClassName=react-router-modal__container] class name to apply to the container itself
+ * @param {String} [props.bodyModalClassName=react-router-modal__modal-open] class name to apply to the <body /> when any modals are shown
+ *
+ * @example <caption>Using default class names</caption>
+ *
+ * <ModalContainer />
+ *
+ * @example <caption>Overriding the default class names</caption>
+ *
+ * <ModalContainer
+ *   bodyModalOpenClassName='modal-open'
+ *   containerClassName='modal-container'
+ *   backdropClassName='modal-backdrop'
+ *   modalClassName='modal'
+ * />
+ *
+ *
+ * @example <caption>DOM structure</caption>
+ * // Note that modals are made "modal" via CSS styles, and end up rendered like the following in the DOM (with two modals, for example):
+ * <div className={containerClassName}>
+ *   <div>
+ *     <div className={backdropClassName} />
+ *     <div className={modalClassName}>
+ *       .. bottom-most modal contents ..
+ *     </div>
+ *   </div>
+ *   <div>
+ *     <div className={backdropClassName} />
+ *     <div className={modalClassName}>
+ *       .. top-most modal contents ..
+ *     </div>
+ *   </div>
+ * </div>
+ *
+ */
 
 export default class ModalContainer extends React.Component {
   props: Props
@@ -175,13 +175,18 @@ export default class ModalContainer extends React.Component {
 
     const modals = this.getSortedModals();
 
-    if (modals.length === 0) {
-      document && document.body && bodyModalOpenClassName && document.body.classList.remove(bodyModalOpenClassName);
-      return null;
+    if (typeof document !== 'undefined') {
+      if (modals.length === 0) {
+        document.body && bodyModalOpenClassName && document.body.classList.remove(bodyModalOpenClassName);
+      }
+      else {
+        document.body && bodyModalOpenClassName && document.body.classList.add(bodyModalOpenClassName);
+      }
     }
 
-    document && document.body && bodyModalOpenClassName && document.body.classList.add(bodyModalOpenClassName);
-
+    if (modals.length === 0) {
+      return null;
+    }
     return (
       <div className={containerClassName}>
         {modals.map(m => <ModalWithBackdrop
