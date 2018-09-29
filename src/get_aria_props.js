ca855d2e1a44a92c924d945affc72f5e5e6cd087
Dave Foley <davidmfoley@gmail.com>
2018-09-29 10:22:49 -0700

    Allow setting role

--- src/get_aria_props.js
+++ src/get_aria_props.js
@@ -2,7 +2,7 @@
 
 const getAriaProps = (props: Object) => {
   const ariaProps = {};
-  const keys = Object.keys(props).filter(p => p.indexOf('aria-') === 0);
+  const keys = Object.keys(props).filter(p => p.indexOf('aria-') === 0 || p === 'role');
   keys.forEach(p => {
     ariaProps[p] = props[p];
   });
